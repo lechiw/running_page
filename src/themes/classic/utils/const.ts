6adfdd3668f135ca9f8e976b8a8f70a28030866d
liw <735437652@qq.com>
2026-07-23 18:02:59 +0800

    Switch to CartoCDN Voyager (free, has labels, works in China)

--- src/themes/classic/utils/const.ts
+++ src/themes/classic/utils/const.ts
@@ -226,11 +226,11 @@ export const INDOOR_COLOR = '#8899aa';
 
 // map tiles vendor, maptiler or mapbox or stadiamaps
 // if you want to use maptiler, set the access token in MAP_TILE_ACCESS_TOKEN
-export const MAP_TILE_VENDOR = 'mapbox';
+export const MAP_TILE_VENDOR = 'mapcn';
 
 // map tiles style name, see MAP_TILE_STYLES for more details
 export const MAP_TILE_STYLE_LIGHT = 'osm-bright';
-export const MAP_TILE_STYLE_DARK = 'light-v11';
+export const MAP_TILE_STYLE_DARK = 'osm-bright';
 
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
